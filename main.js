9cd7ea446a115e26c30c45e3f0ff44dc28cca485
Alday <0266863@students.dadeschools.net>
2019-02-13 14:03:17 -0500

    Started CarGame

---
+++ main.js
@@ -0,0 +1,34 @@
+// VARIABLES
+const car = {
+    make: 'McLaren',
+    model: '720S',
+    color: 'gold',
+    mileage: 0,
+    isWorking: true,
+
+    driveToWork(){
+        alert(`Old Mileage: ${this.mileage}`);
+        this.mileage += 10;
+        alert(`New Mileage: ${this.mileage}`);
+    }
+}
+
+//FUNCTIONS
+//Logs all of our car's current stats to the console
+/*
+function reWriteStats(){
+
+}
+*/
+
+// MAIN PROCESS
+//Captures keyboard input. Depending on the letter pressed it will "call" (execute) different funcctions.
+document.onkeyup = (event) => {
+    //Captures the key press, converts it to lowercase, and saves it to a variable
+    let letter = String.fromCharCode(event.keyCode).toLowerCase();
+
+    if (letter === "d") {
+        car.driveToWork();
+        reWriteStats();
+    }
+}
