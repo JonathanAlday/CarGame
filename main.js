fce8c9647d57a107a6c965ceb93e24f8344cd7f2
Alday <0266863@students.dadeschools.net>
2019-02-15 14:09:44 -0500

    finished CarGame

--- main.js
+++ main.js
@@ -10,16 +10,41 @@ const car = {
         alert(`Old Mileage: ${this.mileage}`);
         this.mileage += 10;
         alert(`New Mileage: ${this.mileage}`);
+    },
+
+    driveAroundUSA(){
+        alert(`Old Mileage: ${this.mileage}`);
+        this.mileage += 7000;
+        alert(`New Mileage: ${this.mileage}`);
+        alert(`New Mileage: ${this.mileage}`);
+        alert(`Car needs a tuneup!`);
+        this.isWorking = false;  
+    },
+
+    getTuneUp(){
+        alert(`Car is fixed and ready to go!`);
+        this.isWorking = true;
+    },
+
+    honk(){
+        alert("Honk! Honk!");
     }
+    
 }
 
 //FUNCTIONS
 //Logs all of our car's current stats to the console
-/*
-function reWriteStats(){
+
+reWriteStat=()=>{
+    console.log(`Make: ${car.make}`);
+    console.log(`Model: ${car.model}`);
+    console.log(`Color: ${car.color}`);
+    console.log(`Mileage: ${car.mileage}`);
+    console.log(`IsWorking: ${car.isWorking}`);
+    console.log(`------------------------------`);
 
 }
-*/
+
 
 // MAIN PROCESS
 //Captures keyboard input. Depending on the letter pressed it will "call" (execute) different funcctions.
@@ -29,6 +54,21 @@ document.onkeyup = (event) => {
 
     if (letter === "d") {
         car.driveToWork();
+        reWriteStats();
+    }
+
+    if (letter === "r") {
+        car.driveAroundUSA();
+        reWriteStats();
+    }
+
+    if (letter === "t") {
+        car.getTuneUp();
+        reWriteStats();
+    }
+
+    if (letter === "h") {
+        car.honk();
         reWriteStats();
     }
 }
